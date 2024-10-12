fc9de6ce02cf9a514d3f5f527764363d7dda1e67
Stefano Montrasio <stefano.montrasio@amaris.com>
2024-10-12 11:40:15 +0200

    TanStack query to cache scraped image url

--- database/startup-scripts/upload-movies.sql
+++ database/startup-scripts/upload-movies.sql
@@ -2,7 +2,7 @@ LOAD DATA INFILE '/var/lib/mysql-files/title.basics.tsv'
 INTO TABLE movies
 FIELDS TERMINATED BY '\t'
 LINES TERMINATED BY '\n'
-IGNORE 1 LINES
+IGNORE 10000000 LINES
 (id, @titleType, @title_value, @originalTitle, @isAdult, @start_year_value, @endYear, @runtime_value, @genres_value)
 SET runtime = NULLIF(@runtime_value, '\N'),
     start_year = NULLIF(@start_year_value, '\N'),
--- frontend/src/components/MoviePoster.tsx
+++ frontend/src/components/MoviePoster.tsx
@@ -1,9 +1,11 @@
-import puppeteer from 'puppeteer-core/lib/esm/puppeteer/puppeteer-core-browser.js';
-import {useEffect, useRef, useState} from "react";
-import Constants from "../utils/constants.ts";
+import {useEffect, useRef} from "react";
 import image from "../assets/image-placeholder.png";
 import {Page} from "puppeteer-core/lib/esm/puppeteer/api/Page.d.ts";
 import classes from "../css/MovieSearch.module.css";
+import {useQuery} from "@tanstack/react-query";
+import Constants from "../utils/constants.ts";
+import {Browser} from "puppeteer-core";
+import puppeteer from "puppeteer-core/lib/esm/puppeteer/puppeteer-core-browser.js";
 
 export interface MoviePosterProps {
   movieTitle: string;
@@ -11,40 +13,60 @@ export interface MoviePosterProps {
 }
 
 export default function MoviePoster(props: MoviePosterProps) {
-  const [moviePosterSrc, setMoviePosterSrc] = useState<string>();
   const browserPage = useRef<Page>();
+  const browserConnection = useRef<Browser>();
+
+  const closeConnection = () => {
+    console.log("Closing connection");
+    if (browserPage.current && !browserPage.current.isClosed()) {
+      browserPage.current.close()
+        .then(() => {
+          if (browserConnection.current && browserConnection.current.connected) {
+            browserConnection.current.close();
+          }
+        });
+    }
+  };
 
   useEffect(() => {
-    getMoviePosterSrc()
-      .then(moviePosterUrl => setMoviePosterSrc(moviePosterUrl))
-      .catch(error => {
-        console.error(`Error while scraping page ${props.imdbUrl}`, error);
-      })
-      .finally(async () => {
-        await browserPage.current?.close();
-      });
-  }, [props.imdbUrl]);
+    return closeConnection
+  }, []);
 
-  const getMoviePosterSrc = async () => {
-    const browser = await puppeteer.connect({
+  const getMoviePosterSrc = async (signal: AbortSignal) => {
+    console.log("opening connection");
+    // @ts-expect-error browserConnection.current could be undefined
+    browserConnection.current = await puppeteer.connect({
       browserWSEndpoint: "ws://localhost:4000"
     });
-    browserPage.current = await browser.newPage();
-    await browserPage.current.setUserAgent(Constants.BROWSER_AGENT);
-    await browserPage.current.goto(props.imdbUrl, {
+    // @ts-expect-error browserPage.current could be undefined
+    browserPage.current = await browserConnection.current?.newPage();
+
+    signal.addEventListener("abort", closeConnection);
+
+    await browserPage.current?.setUserAgent(Constants.BROWSER_AGENT);
+    await browserPage.current?.goto(props.imdbUrl, {
       waitUntil: "domcontentloaded",
     });
-
-    return await browserPage.current.evaluate(() => {
+    return browserPage.current?.evaluate(() => {
       const moviePoster = document.querySelector(".ipc-media--poster-l img") as HTMLImageElement;
       return moviePoster && moviePoster.src;
     });
   };
 
+  const { data } = useQuery({
+    queryFn: ({signal}) =>
+      getMoviePosterSrc(signal)
+        .catch(error => {
+          throw new Error(`Error in scraping page ${props.imdbUrl}: ${error?.message}`);
+        })
+        .finally(closeConnection),
+    queryKey: ["imdb-scraping", props.imdbUrl]
+  });
+
   return (
     <div className={classes.search__moviePoster}>
       <img
-        src={moviePosterSrc || image}
+        src={data || image}
         style={{width: "50px", height: "74px"}}
         alt={`Movie poster of ${props.movieTitle}`}
       />
